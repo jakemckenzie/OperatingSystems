c600d9ff946fa406be197b9c531602e25edfce9d
jakemckenzie <jake314@uw.edu>
2018-10-16 08:01:23 -0700

    added the RIGHT question this time.

---
+++ 3_10.c
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>// The book forgot to include
+#include <sys/wait.h>// these libraries? *tsk tsk*
+int main() {
+    pid_t pid; /* fork a child process */
+    pid = fork();
+    if (pid < 0) { /* error occurred */
+        fprintf(stderr, "Fork Failed");
+        return 1;
+    } else if (pid == 0) { /* child process */
+        execlp("/bin/ls","ls",NULL);
+        printf("LINE J");
+    } else { /* parent process */
+        /* parent will wait for the child to complete */
+        wait(NULL);
+        printf("Child Complete");
+    }
+    return 0;
+}
