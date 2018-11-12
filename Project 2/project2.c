eb3e13f85e9579abf04ec4482f209948360a3000
jakemckenzie <jake314@uw.edu>
2018-11-12 04:48:37 -0800

    os project?

---
+++ Project 2/project2.c
@@ -0,0 +1,168 @@
+ /* 
+  * Jake McKenzie
+  * Operating Systems
+  * Project 2 Produce-Consumer problem in linux
+  * 
+  * INSTRUCTIONS: 
+  * 
+  * In problem we will solve the bounded-buffer problem
+  * with a synchronization solution. The producer produces
+  * items for the bufffer and the consumer takes items from
+  * the buffer consuming them. 
+  */
+
+#include "buffer.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+// semphore full state
+sem_t full; 
+// semphore empty state
+sem_t empty;
+// bounded buffer
+buffer_item buffer[BUFFER_SIZE];
+// pthreaded mutex lock
+pthread_mutex_t mutex;
+// amount of produced items
+volatile int produce; 
+// amount of consumed items
+volatile int consume; 
+// total inventory
+volatile int inventory; 
+// the amount the user specifies to sleep
+volatile int slp;
+// the amount the user specifies to produce
+volatile int prdc;
+// the amount the user specifies to consume
+volatile int cnsm;
+
+void *producer(void *param); 
+void *consumer(void *param);
+int produce_item(buffer_item item);
+int consume_item(buffer_item *item);
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {    
+        fprintf(stderr,"ERROR: ./project2 Please provide a sleep time, producer number and consumer number greater than 1!\n");
+        return -1;
+    } else{
+        if(atoi(argv[1]) < 2 || atoi(argv[2]) < 2 || atoi(argv[3]) < 2){
+            fprintf(stderr,"ERROR: ./project2 Please provide a sleep time, producer number and consumer number greater than 1!\n");
+            return -1;
+        } else{
+            // user defined sleep time
+            slp = atoi(argv[1]);
+            // user defined producer number
+            prdc = atoi(argv[2]);
+	        // user defined consumer number
+            cnsm = atoi(argv[3]);
+            
+            srand(time(NULL));
+            // c does not have a monitor so we accomplish this 
+            // by locking by associating a condition variable with 
+            // a mutex lock. This generates and initiallizes a 
+            // conditional variable and the associated 
+            // mutex lock
+            pthread_mutex_init(&mutex, NULL);
+            sem_init(&empty, 0, BUFFER_SIZE);
+            sem_init(&full, 0, 0);
+            
+            /* initiallize counters to zero */
+            inventory = 0;
+            produce   = 0;
+            consume   = 0;
+
+            // produces the correct number of producer threads
+            pthread_t prdc_t[prdc];
+            int i = 0; 
+            while ( i != prdc) {
+                pthread_create(&prdc_t[i], NULL, producer, NULL);
+                i++;
+            }
+            // produces the correct number of consumer threads.
+            pthread_t cnsm_t[cnsm];
+            int j = 0;
+            
+            while (j != cnsm) {
+                pthread_create(&cnsm_t[j], NULL, consumer, NULL);
+                j++;
+            }
+            // go to sleep
+            sleep(slp);
+            return 0;
+        }
+    } 
+}
+
+void *producer(void *param) {  
+    buffer_item item;
+    while(1){
+        // sleep for random amounts of time based on user definition
+        sleep_rand();
+        item = rand(); 
+        if(produce_item(item)){
+            printf("Error...\n");
+        } else{  
+            printf("Producer id: %u produced %d items\n",(unsigned int)pthread_self(), prdc);
+        }
+    }
+
+}
+
+void *consumer(void *param) {    
+    buffer_item item;
+    while(1){
+        // sleep for random amounts of time based on user definition
+        sleep_rand();
+        if(consume_item(&item)){
+            printf("Error...\n");
+        } else{  
+            printf("Consumer id: %u consumed %d items\n",(unsigned int)pthread_self(), cnsm);
+        }    
+    }
+}
+
+int produce_item(buffer_item item) {
+
+    int flag = 0;
+    sem_wait(&empty);
+    pthread_mutex_lock(&mutex);
+    if(inventory != BUFFER_SIZE) {
+        buffer[produce] = item;
+        produce++;
+        inventory++;
+        flag = 0;
+    } else {
+        flag = -1;
+    }
+    
+    pthread_mutex_unlock(&mutex);
+    sem_post(&full);
+
+    return flag;
+}
+
+int consume_item(buffer_item *item) {
+    int flag;
+    sem_wait(&full);
+    pthread_mutex_lock(&mutex);
+    if(inventory != 0) {
+        *item = buffer[consume];
+        consume++;
+        inventory--;
+        flag = 0;
+    } else {
+        flag = -1;
+    }
+    pthread_mutex_unlock(&mutex);
+    sem_post(&empty);
+
+    return flag;
+}
+
+void sleep_rand(void) {
+    sleep(rand() % slp + 1);
+}
